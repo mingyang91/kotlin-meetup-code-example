7bd307f45dfce5f6dfecf958fbf89294bd8a5c81
明扬 <my@famer.me>
2024-06-30 16:13:02 +0800

    after live

--- ioc/src/main/kotlin/service/OrderService.kt
+++ ioc/src/main/kotlin/service/OrderService.kt
@@ -7,13 +7,13 @@ import java.util.UUID
 @Service
 class OrderService(
     private val orderRepository: OrderRepository,
-//    private val paymentService: PaymentService
+    private val paymentService: PaymentService,
 ) {
     fun cancelOrder(orderId: UUID) {
-        // TODO: 更新订单状态
-//        orderRepository.updateOrder("Cancelled")
-        // TODO: 取消支付
-//        paymentService.cancelPayment()
+        // TODO: 更新订单状态 ordRepo update to Cancelled
+        orderRepository.updateOrder("Cancelled")
+        // TODO: 取消支付 paymentSvc cancel
+        paymentService.cancelPayment()
     }
 
     fun completeOrder() {
